8c366dbc7520cbfb2bcdf2ed71e4a26e06899820
Maria Luísa Mendonça <marialuisamedoliv@gmail.com>
2020-08-31 21:56:45 -0300

    checagem de sintaxe

--- ex01.py
+++ ex01.py
@@ -1,5 +1,12 @@
+#ALUNOS: Fernando Mertinho, RA:
+# Maria Luísa Mendonça RA: 0049545
+
+
 alfabeto = ['P', 'Q', 'R', '(', ')', '~', '$', 'v', '^', '>']
-achei = True
+operando = ['P', 'Q', 'R', '(', ')']
+operadores = ['~', '$', 'v', '^', '>']
+
+i = 0
 op = 0
 
 
@@ -25,6 +32,30 @@ def check_position():
                 print("Formula valida parabens")
                 break
 
+
+def check_operadores():
+
+    for i in range(len(formula)-1):
+        if formula[i] in operadores:
+            if formula[i + 1] in operando:
+                formula[i + 1]
+                print('Nada de errado até agora')
+
+            else:
+                print('Parece que você não atribuiu símbolo a um operador')
+                break
+
+        elif formula[i] in operando:
+            if formula[i + 1] in operadores:
+                formula[i + 1]
+                print('Nada de errado até agora')
+            else:
+                print('Parece que você não atribuiu símbolo a um operador')
+                break
+
+
+
+
 def check_alfabeto():
     if set(formula).intersection(alfabeto):
             check_position()
@@ -44,11 +75,8 @@ while op != 4:
 
         print('REGRAS: \n'
               'O alfabeto usado será: P, Q e R; \n'
-              'Os simbolos proposicionais são:\nNegação = ~\nOu,Or = v\nSe somente se = $\nAnd, Conjução = ^\nSe,Implicação = >'
+              'Os simbolos proposicionais são:\nNegação = ~\nOu,Or = v\nSe somente se = $\nAnd, Conjução = ^\nSe,Implicação = >\n'
               'Pode-se usar apenas uma vez cada símbolo de pontuação;\n'
-              'Os operadores deverão ser informados da seguinte maneira: \n'
-              'Pensei em colocar como se deve inserir os operadores.'
-              'Deve ter alguma biblioteca pra isso'
               'O tamanho máximo de cada fórmula é de 12 caracteres (considerando também os símbolos).')
 
         formula = input('Digite a fórmula: ')
@@ -61,9 +89,12 @@ while op != 4:
 
     if op == 2:
         formula
+
         check_alfabeto()
+        check_operadores()
 
 
-    '''função len calcula o tamanho da formula contando todos os caracteres incluindo ()'''
+
+    #função len calcula o tamanho da formula contando todos os caracteres incluindo ()
     if op == 3:
         print('Tamanho da fórmula é: ', len(formula))
